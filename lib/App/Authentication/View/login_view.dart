8c34bcd3a8f021a1eb04f3080c4bcb2c750d65fb
Sufyian Asghar <sufyianasghar88a@gmail.com>
2023-08-30 16:47:27 +0500

    form validation

--- lib/App/Authentication/View/login_view.dart
+++ lib/App/Authentication/View/login_view.dart
@@ -8,11 +8,18 @@ import 'package:flutter/material.dart';
 import '../../../Common/FormFields/primary_form_field.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+   LoginView({super.key});
+final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+TextEditingController email=TextEditingController();
+TextEditingController password=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     
+      drawer: Drawer(),
+      key: _scaffoldKey,
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Container(
@@ -21,97 +28,119 @@ class LoginView extends StatelessWidget {
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 60,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade100,
-                          spreadRadius: 2,
-                          blurRadius: 3)
-                    ]),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 33,
-                      ),
-                      CustomText(
-                        title: "Welcome to EasyShop",
-                        size: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      CustomText(
-                        title: "Sign in to Continue",
-                        size: 13,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      SizedBox(
-                        height: 35,
-                      ),
-                      PrimaryFormField(),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      PrimaryFormField(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: CustomText(
-                              title: "Forgot Password?",
-                              size: 13,
-                              fontWeight: FontWeight.w500,
+          child: Form(
+            key: formKey,
+
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.shade100,
+                            spreadRadius: 2,
+                            blurRadius: 3)
+                      ]),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 33,
+                        ),
+                        CustomText(
+                          title: "Welcome to EasyShop",
+                          size: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        CustomText(
+                          title: "Sign in to Continue",
+                          size: 13,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        SizedBox(
+                          height: 35,
+                        ),
+                        PrimaryFormField(controller: email,errorMessage: "Email is Required!",),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        PrimaryFormField(controller: password,),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+
+                                _scaffoldKey.currentState!.openDrawer();
+
+
+
+                              },
+                              child: CustomText(
+                                title: "Forgot Password?",
+                                size: 13,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      PrimaryBTN(
-                        onPressed: () {
-                          print("object 1");
-                          Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomeView()),
-                              (route) => false);
-                        },
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                    ],
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        PrimaryBTN(
+                          onPressed: () {
+
+                          if(formKey.currentState!.validate()){
+                            print(email.text);
+                            print(password.text);
+
+
+                             print("object 1");
+                            // Navigator.pushAndRemoveUntil(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (context) => HomeView()),
+                            //     (route) => false);
+
+                          }
+
+
+
+                        
+                          },
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              IconBtn(),
-              SizedBox(
-                height: 20,
-              ),
-              IconBtn(),
-            ],
+                SizedBox(
+                  height: 15,
+                ),
+                IconBtn(),
+                SizedBox(
+                  height: 20,
+                ),
+                IconBtn(),
+              ],
+            ),
           ),
         ),
       )),
--- lib/Common/FormFields/primary_form_field.dart
+++ lib/Common/FormFields/primary_form_field.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 class PrimaryFormField extends StatelessWidget {
-  const PrimaryFormField({super.key});
+  final TextEditingController controller;
+  final String errorMessage;
+
+   PrimaryFormField({super.key,required this.controller,this.errorMessage=""});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      
         keyboardType: TextInputType.number,
+        validator: (value) {
+          
+          if((value==null || value=="") && errorMessage.isNotEmpty){
+          return errorMessage;
+          }else{
+
+         return null;
+
+
+          }
+          
+        },
         decoration: InputDecoration(
           floatingLabelBehavior: FloatingLabelBehavior.always,
           labelText: "Email",
